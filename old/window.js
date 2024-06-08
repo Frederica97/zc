7ffb8ef8312393217146424490e03c1f208b61fc
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2024-06-08 17:55:35 -0400

    zoom

--- old/window.js
+++ old/window.js
@@ -1,11 +1,14 @@
 function setZoom() {
     var screenWidth = window.innerWidth;
-    var zoomPer = 0.8;
+    var zoomPer = 1;
     if (screenWidth <= 1366){
         zoomPer *= 0.9;
     }
     if (screenWidth >= 2000){
-        zoomPer = 1;
+        zoomPer = 1.1;
+    }
+    if (screenWidth >= 2300){
+        zoomPer = 1.25;
     }
     document.body.style.zoom = zoomPer;
 }
