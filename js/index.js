8356bc93902c7ffd841a75126fa96202a4118cc9
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2024-06-09 18:48:44 -0400

    phone scroll

--- js/index.js
+++ js/index.js
@@ -17,181 +17,230 @@ var portfolio_13 = document.getElementById("13");
 var cover = document.getElementById("cover");
 var portfolio = document.getElementById("portfolio");
 var portfolio_set = [
-    portfolio_01,
-    portfolio_02,
-    portfolio_03,
-    portfolio_04,
-    portfolio_05,
-    portfolio_06,
-    portfolio_07,
-    portfolio_08,
-    portfolio_09,
-    portfolio_10,
-    portfolio_11,
-    portfolio_12,
-    portfolio_13
+  portfolio_01,
+  portfolio_02,
+  portfolio_03,
+  portfolio_04,
+  portfolio_05,
+  portfolio_06,
+  portfolio_07,
+  portfolio_08,
+  portfolio_09,
+  portfolio_10,
+  portfolio_11,
+  portfolio_12,
+  portfolio_13
 ];
 function expand(element) {
-    var line = element.querySelector(".mark");
-    if (line) {
-        line.style.height = "150px";
-        line.style.width = "5px";
-        line.style.transition = "all 1s";
-        var info = element.querySelector(".info");
-        info.removeAttribute("hidden");
-        var grid = element.querySelector(".grid");
-        grid.classList.add("grid-active");
-    }
+  var line = element.querySelector(".mark");
+  if (line) {
+    line.style.height = "150px";
+    line.style.width = "5px";
+    line.style.transition = "all 1s";
+    var info = element.querySelector(".info");
+    info.removeAttribute("hidden");
+    var grid = element.querySelector(".grid");
+    grid.classList.add("grid-active");
+  }
 }
 function collapse(element) {
-    var line = element.querySelector(".mark");
-    if (line) {
-        line.style.height = "10px";
-        line.style.width = "10px";
-        line.style.transition = "all 0s";
-        var info = element.querySelector(".info");
-        info.setAttribute("hidden", "true");
-        line.style.transition = "all 0s";
-        var grid = element.querySelector(".grid");
-        grid.classList.remove("grid-active");
-    }
+  var line = element.querySelector(".mark");
+  if (line) {
+    line.style.height = "10px";
+    line.style.width = "10px";
+    line.style.transition = "all 0s";
+    var info = element.querySelector(".info");
+    info.setAttribute("hidden", "true");
+    line.style.transition = "all 0s";
+    var grid = element.querySelector(".grid");
+    grid.classList.remove("grid-active");
+  }
 }
 function collapseOthers(element) {
-    portfolio_set.forEach(function (p) {
-        if (p != element) {
-            collapse(p);
-        }
-    });
+  portfolio_set.forEach(function (p) {
+    if (p != element) {
+      collapse(p);
+    }
+  });
 }
 function indexHover(element) {
-    if (element) {
-        element.addEventListener(
-            "mouseover",
-            function () {
-                var pattern = "repeat(13, 12px";
-                // deal with first image, change the left-bar color
-                if (cover) {
-                    if (element == portfolio_02) {
-                        cover.style.borderLeftColor = "#fff";
-                    } else {
-                        cover.style.borderLeftColor = "#ccd3d9";
-                    }
-                }
-                //collapse others
-                if (element != portfolio_13) {
-                    collapse(portfolio_13);
-                }
-                if (element != portfolio_04) {
-                    collapse(portfolio_04);
-                }
-                if (element != portfolio_03) {
-                    collapse(portfolio_03);
-                }
-                for (var i = 1; i < portfolio_set.length; i++) {
-                    if (portfolio_set[i] == element) {
-                        pattern += " 600px";
-                    } else {
-                        pattern += " 50px";
-                    }
-                }
-                if (portfolio) {
-                    portfolio.style.gridTemplateColumns = pattern + ")";
-                    portfolio.style.transition = "all 1s";
-                }
-                expand(element);
-            },
-            false
-        );
-    }
+  if (element) {
+    element.addEventListener(
+      "mouseover",
+      function () {
+        var pattern = "repeat(13, 12px";
+        // deal with first image, change the left-bar color
+        if (cover) {
+          if (element == portfolio_02) {
+            cover.style.borderLeftColor = "#fff";
+          } else {
+            cover.style.borderLeftColor = "#ccd3d9";
+          }
+        }
+        //collapse others
+        if (element != portfolio_13) {
+          collapse(portfolio_13);
+        }
+        if (element != portfolio_04) {
+          collapse(portfolio_04);
+        }
+        if (element != portfolio_03) {
+          collapse(portfolio_03);
+        }
+        for (var i = 1; i < portfolio_set.length; i++) {
+          if (portfolio_set[i] == element) {
+            pattern += " 600px";
+          } else {
+            pattern += " 50px";
+          }
+        }
+        if (portfolio) {
+          portfolio.style.gridTemplateColumns = pattern + ")";
+          portfolio.style.transition = "all 1s";
+        }
+        expand(element);
+      },
+      false
+    );
+  }
 }
 function indexHoverOver(element) {
-    if (element) {
-        element.addEventListener(
-            "mouseleave",
-            function () {
-                if (cover) {
-                    cover.style.borderLeftColor = "#fff";
-                }
-                if (portfolio) {
-                    portfolio.style.gridTemplateColumns =
-                        "repeat(13, 12px 600px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px)";
-                    portfolio.style.transition = "all 1s";
-                }
-                collapse(element);
-            },
-            false
-        );
-    }
+  if (element) {
+    element.addEventListener(
+      "mouseleave",
+      function () {
+        if (cover) {
+          cover.style.borderLeftColor = "#fff";
+        }
+        if (portfolio) {
+          portfolio.style.gridTemplateColumns =
+            "repeat(13, 12px 600px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px)";
+          portfolio.style.transition = "all 1s";
+        }
+        collapse(element);
+      },
+      false
+    );
+  }
 }
 // click that Contact
 
-function contactPage(){
-    if (portfolio) {
-        portfolio.style.gridTemplateColumns =
-            "repeat(13, 12px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 600px)";
-        portfolio.style.transition = "all 1s";
-    }
-    collapse(portfolio_04);
-    collapse(portfolio_03);
-    expand(portfolio_13);
-    indexHover(portfolio_13);
+function contactPage() {
+  if (portfolio) {
+    portfolio.style.gridTemplateColumns =
+      "repeat(13, 12px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 600px)";
+    portfolio.style.transition = "all 1s";
+  }
+  collapse(portfolio_04);
+  collapse(portfolio_03);
+  expand(portfolio_13);
+  indexHover(portfolio_13);
 }
 
 contactMenu.addEventListener("click", contactPage);
 
-window.onload = function() {
-    const data = localStorage.getItem('setToContact');
-    if (data) {
-        contactPage();
-        localStorage.removeItem('setToContact');
-    }
-}
+window.onload = function () {
+  const data = localStorage.getItem("setToContact");
+  if (data) {
+    contactPage();
+    localStorage.removeItem("setToContact");
+  }
+};
 
 //click that work
 workMenu.addEventListener("click", function () {
-    if (portfolio) {
-        portfolio.style.gridTemplateColumns =
-            "repeat(13, 12px 50px 50px 600px 50px 50px 50px 50px 50px 50px 50px 50px 50px )";
-        portfolio.style.transition = "all 1s";
-    }
-    collapse(portfolio_13);
-    collapse(portfolio_03);
-    expand(portfolio_04);
+  if (portfolio) {
+    portfolio.style.gridTemplateColumns =
+      "repeat(13, 12px 50px 50px 600px 50px 50px 50px 50px 50px 50px 50px 50px 50px )";
+    portfolio.style.transition = "all 1s";
+  }
+  collapse(portfolio_13);
+  collapse(portfolio_03);
+  expand(portfolio_04);
 });
 aboutMenu.addEventListener("click", function () {
-    if (portfolio) {
-        portfolio.style.gridTemplateColumns =
-            "repeat(13, 12px 50px 600px 50px  50px 50px 50px 50px 50px 50px 50px 50px 50px )";
-        portfolio.style.transition = "all 1s";
-    }
-    collapse(portfolio_13);
-    collapse(portfolio_04);
-    expand(portfolio_03);
+  if (portfolio) {
+    portfolio.style.gridTemplateColumns =
+      "repeat(13, 12px 50px 600px 50px  50px 50px 50px 50px 50px 50px 50px 50px 50px )";
+    portfolio.style.transition = "all 1s";
+  }
+  collapse(portfolio_13);
+  collapse(portfolio_04);
+  expand(portfolio_03);
 });
 function indexRefresh() {
-    indexHover(portfolio_02);
-    indexHoverOver(portfolio_02);
-    indexHover(portfolio_03);
-    indexHoverOver(portfolio_03);
-    indexHover(portfolio_04);
-    indexHoverOver(portfolio_04);
-    indexHover(portfolio_05);
-    indexHoverOver(portfolio_05);
-    indexHover(portfolio_06);
-    indexHoverOver(portfolio_06);
-    indexHover(portfolio_07);
-    indexHoverOver(portfolio_07);
-    indexHover(portfolio_08);
-    indexHoverOver(portfolio_08);
-    indexHover(portfolio_09);
-    indexHoverOver(portfolio_09);
-    indexHover(portfolio_10);
-    indexHoverOver(portfolio_10);
-    indexHover(portfolio_11);
-    indexHoverOver(portfolio_11);
-    indexHover(portfolio_12);
-    indexHoverOver(portfolio_12);
-    indexHover(portfolio_13);
-    indexHoverOver(portfolio_13);
+  indexHover(portfolio_02);
+  indexHoverOver(portfolio_02);
+  indexHover(portfolio_03);
+  indexHoverOver(portfolio_03);
+  indexHover(portfolio_04);
+  indexHoverOver(portfolio_04);
+  indexHover(portfolio_05);
+  indexHoverOver(portfolio_05);
+  indexHover(portfolio_06);
+  indexHoverOver(portfolio_06);
+  indexHover(portfolio_07);
+  indexHoverOver(portfolio_07);
+  indexHover(portfolio_08);
+  indexHoverOver(portfolio_08);
+  indexHover(portfolio_09);
+  indexHoverOver(portfolio_09);
+  indexHover(portfolio_10);
+  indexHoverOver(portfolio_10);
+  indexHover(portfolio_11);
+  indexHoverOver(portfolio_11);
+  indexHover(portfolio_12);
+  indexHoverOver(portfolio_12);
+  indexHover(portfolio_13);
+  indexHoverOver(portfolio_13);
 }
 indexRefresh();
+
+var totalScroll = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener(
+    "wheel",
+    (e) => {
+      totalScroll += e.deltaY;
+      ScrollNow(totalScroll);
+    },
+    true
+  );
+});
+
+function ScrollNow(totalScroll) {
+  const indexProject = parseInt((totalScroll + 700) / 300);
+  if (indexProject > 2) {
+    if (indexProject > portfolio_set.length - 1) {
+      indexProject = 13;
+    }
+    let index = `${indexProject}`;
+
+    if (indexProject < 10) {
+      index = "0" + index;
+    }
+    const element = document.getElementById(index);
+    var pattern = "repeat(13, 12px";
+    // deal with first image, change the left-bar color
+    if (cover) {
+      if (element == portfolio_02) {
+        cover.style.borderLeftColor = "#fff";
+      } else {
+        cover.style.borderLeftColor = "#ccd3d9";
+      }
+    }
+    for (var i = 1; i < portfolio_set.length; i++) {
+      if (portfolio_set[i] == element) {
+        pattern += " 600px";
+      } else {
+        pattern += " 50px";
+      }
+    }
+    if (portfolio) {
+      portfolio.style.gridTemplateColumns = pattern + ")";
+      portfolio.style.transition = "all 1s";
+    }
+    collapseOthers(element);
+    expand(element);
+  }
+}
